15515d20255470b52d637c56f0c053f6c5bf35e0
Kapil <kapil@Kapils-Mac-mini.local>
2026-05-19 11:34:10 +0530

    add

---
+++ lib/screens/student_profile_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StudentProfilePage extends StatelessWidget {
+  const StudentProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
